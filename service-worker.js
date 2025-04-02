89e7629a24c2c26f0dff97cc632fd9340587f522
Tu Nombre <sasogu@gmail.com>
2025-04-02 22:20:49 +0200

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'peso-cache-v1.0.5'; // Cambia la versión aquí
+const CACHE_NAME = 'peso-cache-v1.0.6'; // Cambia la versión aquí
 
 self.addEventListener('install', e => {
   e.waitUntil(
